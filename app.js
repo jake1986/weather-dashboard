961287228d888263532f44ce264152ec68167814
Jake Rhoads <jake.rhoads@gmail.com>
2020-03-20 12:11:28 -0400

    reformatted date with .split()

--- app.js
+++ app.js
@@ -1,14 +1,89 @@
 $(document).ready(function () {
 
+    // Change button to ID
     var searchBtn = $(".searchBtn");
-    var newCity = $(".newCity");
+    // var newCity = $(".newCity");
 
     searchBtn.on("click", function (event) {
         event.preventDefault();
         var cityInput = $(".city-input").val();
         console.log(cityInput);
         var cityData = $(".city-data");
+
         cityData.text(cityInput);
+
+        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=2c91858cdc5732c71063ad682162dea9";
+
+        $.ajax({
+            url: currentQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var cityData = $(".city-data");
+            cityData.attr("style", "margin-left: 5px;");
+            cityData.text(response.name);
+            var iconcode = response.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            var sideIcon = $(".sideIcon").attr("src", iconurl);
+            sideIcon.append(iconurl);
+            var temp = $(".temp").attr("style", "margin-left: 5px;").text("Temperature: " + response.main.temp + " F");
+            cityData.append(humid, windSpeed);
+            var humid = $(".humidity").attr("style", "margin-left: 5px;").text("Humidty: " + response.main.humidity + " %");
+            var windSpeed = $(".wind-speed").attr("style", "margin-left: 5px;").text("Wind Speed: " + response.wind.speed + " MPH");
+
+        });
+
+        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=2c91858cdc5732c71063ad682162dea9";
+
+        $.ajax({
+            url: fiveDayQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var forecast5 = $(".container-footer");
+            var row = $("<div class=\"row\">");
+            forecast5.append(row);
+
+
+
+            for (var i = 0; i < response.list.length; i++) {
+
+                if (response.list[i].dt_txt.indexOf("18:00:00") !== -1) {
+
+
+                    var column = $("<div class=\"col-md-2\">");
+                    var card = $("<div>").addClass("card");
+                    var cardBody = $("<div>").addClass("card-body").attr("style", "background: lightblue;")
+                    // cardBody.attr("width", "20%");
+                    var cardTitle = $("<h5>").addClass("card-title");
+                    var temp = $("<p>").addClass("temp").text("Temperature: " + response.list[i].main.temp + " F");
+                    var humid = $("<p>").addClass("humid").text("Humidity: " + response.list[i].main.humidity + " %");
+                    var iconcode = response.list[i].weather[0].icon;
+                    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                    var weather = $("<img>").addClass("weather").attr("src", iconurl);
+                    var dateTime = response.list[i].dt_txt;
+                    var dateTimeList = dateTime.split(' ');
+                    var date = dateTimeList[0];
+                    var splitDate = date.split('-');
+                    var formattedDate = splitDate[1] + "/" + splitDate[2] + "/" + splitDate[0];
+                    cardTitle.text(formattedDate);
+                    var pOne = $("<p>").addClass("card-text");
+                    cardBody.append(cardTitle, pOne, weather, temp, humid);
+                    card.append(cardBody);
+                    forecast5.append(card);
+                    forecast5.append(row);
+                    forecast5.append(column);
+                }
+
+            }
+
+
+
+
+
+        });
 
         var newCity = $(".newCity");
         var cityInput = $(".city-input").val();
@@ -22,77 +97,17 @@ $(document).ready(function () {
         var newCity = localStorage.getItem("city-input");
 
 
+
+
     });
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Richmond&appid=2c91858cdc5732c71063ad682162dea9"
+    // var cityInput = $(".city-input").val();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
 
-        var cityData = $(".city-data");
-        cityData.attr("style", "margin-left: 5px;");
-        cityData.text(response.name);
-        var iconcode = response.weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        var sideIcon = $(".sideIcon").attr("src", iconurl);
-        sideIcon.append(iconurl);
-        var temp = $(".temp").attr("style", "margin-left: 5px;").text("Temperature: " + response.main.temp);
-        cityData.append(humid, windSpeed);
-        var humid = $(".humidity").attr("style", "margin-left: 5px;").text("Humidty: " + response.main.humidity);
-        var windSpeed = $(".wind-speed").attr("style", "margin-left: 5px;").text("Wind Speed: " + response.wind.speed);
-        
-    });
 
     // var cities = "Richmond";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Richmond&appid=2c91858cdc5732c71063ad682162dea9";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        
-        var forecast5 = $(".container-footer");
-        var row = $("<div class=\"row\">");
-        forecast5.append(row);
-        
-        
-       
-        for (var i = 0; i < response.list.length; i++) {
-        
-            if (response.list[i].dt_txt.indexOf("18:00:00") !== -1) {
-                
-               
-                var column = $("<div class=\"col-md-2\">");
-                var card = $("<div>").addClass("card");
-                var cardBody = $("<div>").addClass("card-body");
-                // cardBody.attr("width", "20%");
-                var cardTitle = $("<h5>").addClass("card-title");
-                var temp = $("<p>").addClass("temp").text("Temperature: " + response.list[i].main.temp);
-                var humid = $("<p>").addClass("humid").text("Humidity: " + response.list[i].main.humidity);
-                var iconcode = response.list[i].weather[0].icon;
-                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-                var weather = $("<img>").addClass("weather").attr("src", iconurl);
-                cardTitle.text(response.list[i].dt_txt);
-                var pOne = $("<p>").addClass("card-text"); 
-                cardBody.append(cardTitle, pOne, weather, temp, humid);
-                card.append(cardBody);
-                forecast5.append(card);
-                forecast5.append(row);
-                forecast5.append(column);
-            }
-            
-        }
-
-      
-
-
-        
-    });
 
     // <div class="card" style="width: 18rem;">
     //     <div class="card-body">
